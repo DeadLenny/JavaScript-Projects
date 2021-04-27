3e6ad044aa214bd4fa6b45625ffbab24edfe70d1
tansing <83265777+tansing@users.noreply.github.com>
2021-04-27 01:23:50 -0400

    Add files via upload

---
+++ Hello-World-and-Sum-Program/js/index.js
@@ -0,0 +1,24 @@
+var name="Tanvir"
+
+var course="CIT 140000"
+
+var something="Hello there! My name is " +name;
+var csomething="Welcome to "+ course;
+
+document.querySelector(".name").innerHTML=something;
+document.querySelector(".course").innerHTML=csomething;
+
+
+
+var num1=prompt("Choose a number");
+var num2=prompt("Choose a number");
+
+
+
+
+var sum =Number(num1)+Number(num2);
+
+var sum2="The Sum is:"+sum;
+
+
+document.querySelector(".sum").innerHTML=sum2;
